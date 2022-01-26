13bfeedde20530bb07e97516c8be099159e1b9ba
Aniket Bhattacharjee <appsoftlimited@gmail.com>
2022-01-26 22:35:22 +0530

    Create example.java

---
+++ src/java/basics/example.java
@@ -0,0 +1,6 @@
+// withId(R.id.my_view) is a ViewMatcher
+// click() is a ViewAction
+// matches(isDisplayed()) is a ViewAssertion
+onView(withId(R.id.my_view))
+    .perform(click())
+    .check(matches(isDisplayed()));
